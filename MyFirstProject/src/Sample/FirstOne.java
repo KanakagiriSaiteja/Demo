bfbbe3dbe9a41a2268217a8253fa5c313001d795
Saiteja <skanakag@cisco.com>
2015-10-28 22:04:10 +0530

    first commit

---
+++ MyFirstProject/src/Sample/FirstOne.java
@@ -0,0 +1,19 @@
+package Sample;
+
+import java.io.IOException;
+
+public class FirstOne {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter Numbers");
+		int a = System.in.read();
+		int b = System.in.read();
+		int c=a+b;
+		System.out.println("The result is "+c);
+		
+
+	}
+
+}
